5395f20d8f0d48edd0cd621e77a4eb3121184912
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-04-06 17:42:40 +0200

    js wip

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-alert("Hello World!");
+// alert("Hello World!");
+
+// Consegna 
+// Visualizzare in pagina 5 numeri casuali (in un range tra 1 e 100). Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+////////////////////////////////////
+// Flowchart
+
+// 1. Cosa devo fare?
+// Visualizza in pagina 5 numeri casuali
+
+
+// Questo genera un array con 5 numeri random
+const numeri = 5;
+let numeriInseriti = [];
+
+
+function stampaNum(numeri){
+
+  for(i = 0; i < numeri; i++){
+    const numRandom = Math.floor(Math.random() * 10 + 1);
+    console.log('stampa ', numRandom);
+    numeriInseriti.push(numRandom);
+  }
+  
+}
+stampaNum(numeri);
+console.log(numeriInseriti);
+ 
+
+////////////////////////////////////
